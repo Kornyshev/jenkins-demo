7190df6343b5587d30f50c0eaeb25d1b18ab40a8
Evgeniy_Kornyshev <kornyshev@ya.ru>
2025-03-18 18:00:30 +0300

    Init

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void failingTest() {
+        Assertions.assertTrue(false);
+    }
+
+}
